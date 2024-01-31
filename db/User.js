c260c24023f5cb0f012ae2c834f1484cb36f9112
Harshal-creator <harshalshelar8251@gmail.com>
2024-01-31 19:14:24 +0530

    register form fields added

--- db/User.js
+++ db/User.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String
+    password: String,
+    phoneNumber: String,
+    address: String,
+    education: String,
 });
 
 module.exports = mongoose.model("users", userSchema);
